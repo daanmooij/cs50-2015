79f9d10fdf19b5a448b4e1796830466c7bdd235c
Daan Mooij <daanmooij@gmail.com>
2016-03-03 23:31:07 +0100

    Adjust spacing

--- problem-set-1/mario.c
+++ problem-set-1/mario.c
@@ -12,22 +12,22 @@ int main(void)
     }
     while(height < 0 || height > 23);
     
-    int space = height-1;
+    int space = height - 1;
     int hash = 2;
     
-    for(int i=0;i<height;i++)
+    for(int i = 0; i < height; i++)
     {  
-        for(int x=0;x<space;x++)
+        for(int x = 0; x < space; x++)
         {
             printf(" ");
         }
-        for(int y=0;y<(hash);y++)
+        for(int y = 0; y < (hash); y++)
         {
             printf("#");  
         }
         hash += 1;
         space -= 1;
         
-        printf("\n" );
+        printf("\n");
     }
 }
